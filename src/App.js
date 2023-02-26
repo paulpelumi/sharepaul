3585a3ffcc307964ebd0336294571ed2abc26999
“Paul <paulpelumi@gmail.com>
2023-02-26 21:29:33 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Login from "./components/Login";
+
+
+const App = () => {
+  
+  return (
+    <div>
+         
+      <Login />
+    </div>
+    
+  )
+};
+
+export default App
